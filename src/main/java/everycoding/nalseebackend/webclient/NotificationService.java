6fdd0694f37e6def3b5d0c936eb064ad2eaff6a7
JamieJay <jwjh1205@naver.com>
2024-04-05 16:52:38 +0900

    Update NotificationService.java

--- src/main/java/everycoding/nalseebackend/webclient/NotificationService.java
+++ src/main/java/everycoding/nalseebackend/webclient/NotificationService.java
@@ -1,32 +1,32 @@
-package everycoding.nalseebackend.webclient;
+// package everycoding.nalseebackend.webclient;
 
-import everycoding.nalseebackend.webclient.dto.EventDto;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+// import everycoding.nalseebackend.webclient.dto.EventDto;
+// import org.springframework.stereotype.Service;
+// import org.springframework.web.reactive.function.client.WebClient;
+// import reactor.core.publisher.Mono;
 
-import java.time.LocalDateTime;
+// import java.time.LocalDateTime;
 
-@Service
-public class NotificationService {
+// @Service
+// public class NotificationService {
 
-    private final WebClient webClient;
+//     private final WebClient webClient;
 
-    public NotificationService(WebClient.Builder webclientBuilder) {
-        this.webClient = webclientBuilder.baseUrl("http://localhost:8090/").build();
-    }
+//     public NotificationService(WebClient.Builder webclientBuilder) {
+//         this.webClient = webclientBuilder.baseUrl("http://localhost:8090/").build();
+//     }
 
-    public Mono<Void> sentEvent(String message, Long receiverId, Long senderId, LocalDateTime createAt, String senderImage) {
-        return webClient.post()
-                .uri("/events")
-                .bodyValue(EventDto.builder()
-                        .message(message)
-                        .receiverId(receiverId)
-                        .senderId(senderId)
-                        .createAt(createAt)
-                        .senderImage(senderImage))
-                .retrieve()
-                .bodyToMono(Void.class);
-    }
+//     public Mono<Void> sentEvent(String message, Long receiverId, Long senderId, LocalDateTime createAt, String senderImage) {
+//         return webClient.post()
+//                 .uri("/events")
+//                 .bodyValue(EventDto.builder()
+//                         .message(message)
+//                         .receiverId(receiverId)
+//                         .senderId(senderId)
+//                         .createAt(createAt)
+//                         .senderImage(senderImage))
+//                 .retrieve()
+//                 .bodyToMono(Void.class);
+//     }
 
-}
+// }
