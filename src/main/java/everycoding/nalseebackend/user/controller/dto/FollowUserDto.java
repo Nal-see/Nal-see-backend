66dbe57c7492366f21e215c140fe457b64e4c2ae
Seungmok1 <kww4215@naver.com>
2024-04-19 11:21:42 +0900

    feat: 팔로잉, 팔로워 리스트

---
+++ src/main/java/everycoding/nalseebackend/user/controller/dto/FollowUserDto.java
@@ -0,0 +1,12 @@
+package everycoding.nalseebackend.user.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FollowUserDto {
+    private Long userId;
+    private String username;
+    private String picture;
+}
